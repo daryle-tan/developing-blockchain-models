4247b9d96d9b70f049658869184218eddba45388
daryle <daryle.tan@gmail.com>
2022-11-11 21:39:54 -0600

    next Setting up express

---
+++ README.md
@@ -0,0 +1,1 @@
+npm install js-sha256 to create hash
---
+++ app.js
@@ -0,0 +1,16 @@
+const Block = require("./block");
+const Blockchain = require("./blockchain");
+const Transaction = require("./transaction");
+
+let transaction = new Transaction("Mary", "John", 100);
+let genesisBlock = new Block();
+let blockchain = new Blockchain(genesisBlock);
+
+let block = blockchain.getNextBlock([transaction]);
+blockchain.addBlock(block);
+
+let anotherTransaction = new Transaction("Steven", "Brian", 500);
+let block1 = blockchain.getNextBlock([anotherTransaction, transaction]);
+blockchain.addBlock(block1);
+
+console.log(blockchain);
